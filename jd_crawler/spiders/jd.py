dd9bd42a04d0d62b8114c6dfb755d84f2c793de1
my-github-my-github <2368767212@qq.com>
2019-07-31 22:53:10 +0800

    test

--- jd_crawler/spiders/jd.py
+++ jd_crawler/spiders/jd.py
@@ -5,7 +5,9 @@ import scrapy
 class JdSpider(scrapy.Spider):
     name = 'jd'
     allowed_domains = ['jd.com']
-    start_urls = ['http://jd.com/']
+    start_urls = ['https://sclub.jd.com/comment/productPageComments.action?callback=fetchJSON_comment98vv1174&productId=100003150357&score=0&sortType=5&page=0&pageSize=10&isShadowSku=0&fold=1']
+
 
     def parse(self, response):
-        pass
+        text = response.text
+        print(text)
